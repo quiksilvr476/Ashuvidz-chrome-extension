1ef067fbcf739ef876b0866dc1dbf8cf154b867c
vyrtualsynthese <12Merde21yuki>
2018-04-17 01:41:43 +0200

    fix

---
+++ README.md
@@ -0,0 +1,30 @@
+# Ashuvidz Firefox add-on
+
+This is a simple Firefox Quantum add-on for the Youtube and Twitch channel Ashuvidz.
+On the normal state, when the channel is not live clicking it display a simple icone and redirect to youtube channel if you click on it.
+If the channel goes live, you got a notification and the icon change. Clicking on both redirect you to the Twitch channel.
+
+## Getting Started
+
+You juste need your favorite editor and Firefox Quantum or above to test the extension.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **Valentin RONTEIX** - *Initial idea* - [VyrtualSynthese](https://github.com/vyrtualsynthese)
+* **Diu** - *Local Storage and Badges* - [diuNguyen](https://github.com/diuNguyen)
+* **Yihohu** - *Soft Cap logic, code review, manifest editing* - [yihohu](https://github.com/yihohu)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## Acknowledgments
+
+* This was stared during the Firefox extension hackaton in Paris Mozila.
+
+## Road Map
+
+* We should find a solution to hide the api ID and make the application more easy to hack to be used by any Twitch streaming wanting his own twitch/youtube plugin.
+* Use of Youtube Api to make notification on new video published and badges to show how many video to watch.
--- background.js
+++ background.js
@@ -1,31 +1,136 @@
+(
+    () => {
 
-function twitchCheck(){$.getJSON("https://api.twitch.tv/kraken/streams/Ashuvidz?client_id=yufsibfdh5gwli5nzdj01ceghc1ubv", function(channel) {
-  //live is offline Youtube player appened
-  if (channel["stream"] == null) {
-    console.log("Stream is offline");
+        // Bootstraps the plugin.
+        function linkManager(item) {
+            if (!item.linkUrl || !item.isLive) {
+                chrome.storage.local.set({
+                    linkUrl:  `https://youtube.com/${twitchUserName}/`,
+                    isLive: "0"
+                });
+                local = item;
+                linkManager();
+            } else {
+                local = item;
+                console.log(local);
+                twitchCheck();
+            }
+        }
 
-    linkUrl = "http://youtube.com/ashuvidz/";
-    chrome.browserAction.setIcon({path: "images/offline.png"});
-    console.log("set icon offline");
+        // This is the actual logic of the plugin.
+        function twitchCheck(){
 
-    //live is online Twitch player
-  } else {
-      linkUrl = "http://twitch.tv/ashuvidz/";
-      chrome.browserAction.setIcon({path: "images/online.png"});
-      console.log("set icon online");
+            // We first get data from Twitch Using Helix API using new nodejs fetch methode
+            // Setting the API Request to Twitch
+            const twitchUrl = `https://api.twitch.tv/helix/streams?user_login=${twitchUserName}`;
+
+
+            const twitchApiGetter = {
+                method: 'GET',
+                headers: { "Client-ID": twitchApiKey }
+            };
+
+            // Fetching data from twitch API
+            fetch(twitchUrl, twitchApiGetter).then(( response ) => {
+                return response.json()}).then( (channel)=> {
+                    //live is offline Youtube player appened
+                    if (channel.data.length <= 0) {
+
+                        // If status changed, change status back to not streaming and save it in local storage without notifications.
+                        chrome.browserAction.setIcon({path: "images/offline.png"});
+
+                        // If status didn't changed do Nothing
+                        if (local.isLive === '0') return;
+
+                        chrome.storage.local.set({
+                            isLive:  "0",
+                            linkUrl: `https://youtube.com/${twitchUserName}/`
+                        }, () => {
+                            buffer = chrome.storage.local.get(["isLive", "linkUrl"], () => {
+                                linkManager();
+                            });
+                        });
+
+                    } else {
+
+                        // If live, append the link to Twitch and change images
+                        chrome.browserAction.setIcon({path: "images/online.png"});
+
+                        if (local.isLive === '1') return;
+
+                        const twitchGameUrl = 'https://api.twitch.tv/helix/games?id=' + channel.data[0].game_id;
+
+                        fetch(twitchGameUrl, twitchApiGetter).then(( response ) => {
+                            return response.json()}).then( (game)=> {
+
+                           if (game.data.length <= 0) {
+                               return;
+                           }
+
+                            let twitchGameName = game.data[0].name;
+                            const twitchUserUrl = 'https://api.twitch.tv/helix/users?login=' + twitchUserName;
+
+                            fetch(twitchUserUrl, twitchApiGetter).then(( response ) => {
+                                return response.json()}).then( (user)=> {
+
+                                if (user.data.length <= 0) {
+                                    return;
+                                }
+                                // This sends a notification
+
+                                chrome.notifications.create('notifTwitchOffline', {
+                                    'type': 'basic',
+                                    'title': channel.data[0].title,
+                                    'message': `${twitchUserName} est en stream sur ${twitchGameName}`,
+                                    'iconUrl': user.data[0].profile_image_url
+                                });
+
+                                // Then update the local variable and get it again to ensure the promise is fired.
+                                chrome.storage.local.set({
+                                    isLive: "1",
+                                    linkUrl: `http://twitch.tv/${twitchUserName}/`
+                                });
+                                buffer = chrome.storage.local.get(["isLive", "linkUrl"]);
+                                buffer.then(linkManager, onError);
+                            })
+                        });
+                    }
+                }
+            );
+        }
+
+        const twitchApiKey = "b90nfoacg9807542cq15o2qbv2g05q";
+        const twitchUserName = "ashuvidz";
+
+        // This starts the plugin by geeting the saved previous status.
+        let local;
+        let buffer = chrome.storage.local.get(['isLive', 'linkUrl']);
+        buffer.then(linkManager, onError);
+
+        // Borwser Event Listeners. This keep application running after bootstrap.
+        // This create the delay that trigger a new check every minutes
+        chrome.alarms.create('twitchAlarm', {delayInMinutes: 1, periodInMinutes: 1});
+
+        // This manage the behavior when an Alarm Event is Fired
+        chrome.alarms.onAlarm.addListener(() => {
+            buffer.then(twitchCheck);
+        });
+
+        // This manager the behavior when plugin icon is clicked
+        chrome.browserAction.onClicked.addListener(() =>
+        {
+            chrome.tabs.create({ url: local.linkUrl });
+        });
+
+        // This manage the onclick behavior over the notification.
+        chrome.notifications.onClicked.addListener(() => {
+            chrome.tabs.create({ url: local.linkUrl });
+        });
+
+        /*function increment() {
+            chrome.browserAction.setBadgeText({text: (13).toString()});
+        }
+
+        increment()*/
     }
-})};
-var linkUrl = "";
-twitchCheck();
-
-chrome.alarms.create('twitchAlarm', {delayInMinutes: 5, periodInMinutes: 5});
-
-chrome.alarms.onAlarm.addListener(function( twitchAlarm ) {
-  twitchCheck();
-});
-
-chrome.browserAction.onClicked.addListener(function(activeTab)
-{
-    var newURL = linkUrl;
-    chrome.tabs.create({ url: newURL });
-});
+)();
